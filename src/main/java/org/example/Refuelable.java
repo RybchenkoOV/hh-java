118c948ff134e7eb4ce856043875341ddf40579a
RybchenkoOV <ribus@mail.ru>
2022-09-04 23:26:34 +0300

    Lesson 1.

---
+++ src/main/java/org/example/Refuelable.java
@@ -0,0 +1,8 @@
+package org.example;
+
+/**
+ * @author oleg on 04.09.2022
+ */
+public interface Refuelable {
+  double refuel(double time);
+}
---
+++ src/main/java/org/example/Steerable.java
@@ -0,0 +1,9 @@
+package org.example;
+
+/**
+ * @author oleg on 04.09.2022
+ */
+public interface Steerable {
+  void canTurnLeftRight(boolean canDoThis);
+  void canMoveForwardBackward(boolean canDoThis);
+}
