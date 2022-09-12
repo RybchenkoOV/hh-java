c1fb55c9b7926fcba225800b298d8192c32794a8
RybchenkoOV <ribus@mail.ru>
2022-09-12 17:40:07 +0300

    Revert "lesson 4"
    This reverts commit 6e6dbccbb1ba653e37cb1caf2edb40fe14dfdbff.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,42 +1,13 @@
 package org.example;
 
-import java.util.Objects;
-import java.util.Scanner;
-
-public class App {
-  public static void main(String[] args) throws WrongLoginException, WrongPasswordException {
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Ввдите логин: ");
-    String inputLogin = scanner.nextLine();
-    System.out.println("Ввдите пароль: ");
-    String inputPassword = scanner.nextLine();
-    System.out.println("Повторите пароль: ");
-    String inputRepeatPassword = scanner.nextLine();
-
-    System.out.println("Результат выполнения: " + authorize(inputLogin, inputPassword, inputRepeatPassword));
-
-  }
-
-  private static boolean authorize(String login, String password, String confirmPassword) throws WrongLoginException, WrongPasswordException {
-    String regEx = "[a-zA-Z0-9_]{1,20}";
-
-    try {
-      if (login.matches(regEx)) {
-        System.out.println("Логин соответствует политике ввода!");
-      } else throw new WrongLoginException("Введен недопустимый символ");
-      if (password.matches(regEx)) {
-        System.out.println("Пароль соответствует политике ввода!");
-      } else throw new WrongPasswordException();
-      if (Objects.equals(confirmPassword, password)) {
-        System.out.println("Пароли совпали!");
-      } else System.out.println("Пароли не совпадают!");
-    } catch (WrongLoginException e) {
-      e.printStackTrace();
-    } catch (WrongPasswordException e) {
-      e.printStackTrace();
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
     }
-
-    return (login.matches(regEx) && password.matches(regEx) && Objects.equals(confirmPassword, password));
-  }
 }
-
